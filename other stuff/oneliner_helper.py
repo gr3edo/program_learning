723babfc45561903e50f5e84ae1800f9880c9e3b
Ivan <i.mitroshyn@synergetica.net>
2016-10-09 18:36:55 +0300

    oneliners

---
+++ other stuff/oneliner_helper.py
@@ -0,0 +1,6 @@
+print (sum(array[i] for i in range(len(array)) if i % 2 == 0)*array[-1] if array != [] else 0)
+---
+s = "".join([x for x in text if x.isupper()])
+---
+return '111' in "".join(map(lambda word: str(int(word.isalpha())), words.split(" ")))
+---
